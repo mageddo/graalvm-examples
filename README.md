8480a6f6c7fac7de7c06c2ea2e673d48916abfe3
Elvis de Freitas <edigitalb@gmail.com>
2019-01-25 00:05:29 -0200

    setup readme

---
+++ README.md
@@ -0,0 +1,5 @@
+**Not working yet**
+
+```bash
+./gradlew build nativeImage
+```
---
+++ src/main/java/com/mageddo/springbootgraalvm/GraalVmStarter.java
@@ -0,0 +1,11 @@
+package com.mageddo.springbootgraalvm;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GraalVmStarter {
+	public static void main(String[] args) {
+		SpringApplication.run(GraalVmStarter.class);
+	}
+}
