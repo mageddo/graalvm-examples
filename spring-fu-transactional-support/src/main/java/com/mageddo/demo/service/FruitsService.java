791a244ad1565cd1de2479bfe957b05e52ece520
Elvis de Freitas <edigitalb@gmail.com>
2019-03-12 01:07:55 -0300

    sleeping, making some time to test transactional support

--- spring-fu-transactional-support/src/main/java/com/mageddo/demo/service/FruitsService.java
+++ spring-fu-transactional-support/src/main/java/com/mageddo/demo/service/FruitsService.java
@@ -3,6 +3,8 @@ package com.mageddo.demo.service;
 import com.mageddo.demo.dao.FruitsDAO;
 import com.mageddo.demo.entity.FruitEntity;
 import com.mageddo.rawstringliterals.Rsl;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -13,6 +15,7 @@ import java.util.List;
 @Rsl
 public class FruitsService {
 
+	private final Logger logger = LoggerFactory.getLogger(getClass());
 	private final FruitsDAO fruitsDAO;
 
 	public FruitsService(FruitsDAO fruitsDAO) {
@@ -22,6 +25,18 @@ public class FruitsService {
 	@Transactional
 	public List<FruitEntity> getFruits()  {
 		fruitsDAO.traceSelect();
+		for (int i = 1; i <= 10; i++) {
+			logger.info("sleeping, second={}", i);
+			sleep();
+		}
 		return fruitsDAO.getFruits();
+	}
+
+	private void sleep() {
+		try {
+			Thread.sleep(1000);
+		} catch (InterruptedException e) {
+			throw new RuntimeException(e);
+		}
 	}
 }
