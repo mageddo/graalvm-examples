9f92f8cc2c77c2b0b8d74e686666af246941ea94
Elvis de Freitas <edigitalb@gmail.com>
2019-03-19 23:12:21 -0300

    setup thymeleaf

---
+++ micronaut/src/main/java/com/mageddo/micronaut/controller/ViewsController.java
@@ -0,0 +1,29 @@
+package com.mageddo.micronaut.controller;
+
+import io.micronaut.http.HttpResponse;
+import io.micronaut.http.annotation.Controller;
+import io.micronaut.http.annotation.Get;
+import io.micronaut.views.ModelAndView;
+import io.micronaut.views.View;
+
+import static io.micronaut.core.util.CollectionUtils.mapOf;
+import static io.micronaut.http.HttpResponse.ok;
+
+@Controller("/views")
+public class ViewsController {
+
+	@Get
+	@View("home")
+	public HttpResponse index() {
+		return ok(mapOf("loggedIn", true, "username", "sdelamo"));
+	}
+
+	@Get("/modelAndView")
+	ModelAndView modelAndView() {
+		return new ModelAndView(
+			"home",  mapOf("name", "Elvis")
+		);
+	}
+
+
+}
