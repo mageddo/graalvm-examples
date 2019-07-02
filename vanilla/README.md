263cb23d640451f8913a2abeae5c9767a7c4ec46
elfreitas <edigitalb@gmail.com>
2019-07-02 08:21:20 -0300

    setup vanilla sample

---
+++ vanilla/README.md
@@ -0,0 +1,7 @@
+Building a full automated graalvm binary using gradle
+
+```bash
+$ ./gradlew build nativeImage &&\
+  ./build/graal/graalvm-vanilla 
+08:20:02.617 [main] INFO com.mageddo.Application - Hello World!!!
+```
---
+++ vanilla/src/main/java/com/mageddo/Application.java
@@ -0,0 +1,13 @@
+package com.mageddo;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class Application {
+
+	private static final Logger LOG = LoggerFactory.getLogger(Application.class);
+
+	public static void main(String[] args) {
+		LOG.info("Hello World!!!");
+	}
+}
