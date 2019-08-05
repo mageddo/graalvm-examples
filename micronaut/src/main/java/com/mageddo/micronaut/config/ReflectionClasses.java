d0b0ddfed59eb23070e0ee413036789411f4f7fc
Elvis de Freitas <edigitalb@gmail.com>
2019-08-05 18:27:53 -0300

    upgrading graalvm and micronaut

--- micronaut/src/main/java/com/mageddo/micronaut/config/ReflectionClasses.java
+++ micronaut/src/main/java/com/mageddo/micronaut/config/ReflectionClasses.java
@@ -1,73 +1,57 @@
 package com.mageddo.micronaut.config;
 
+import com.mageddo.bookmarks.utils.ThymeleafUtils;
+import com.mageddo.common.graalvm.SubstrateVM;
 import com.oracle.svm.core.annotate.AutomaticFeature;
-import org.graalvm.nativeimage.Feature;
-import org.graalvm.nativeimage.RuntimeReflection;
+import org.flywaydb.core.internal.logging.javautil.JavaUtilLogCreator;
+import org.graalvm.nativeimage.hosted.Feature;
+import org.springframework.transaction.TransactionDefinition;
+import org.thymeleaf.standard.expression.AdditionExpression;
+import org.thymeleaf.standard.expression.EqualsExpression;
+import org.thymeleaf.standard.expression.NotEqualsExpression;
 
-import java.lang.reflect.Constructor;
-import java.lang.reflect.Method;
 import java.sql.Statement;
 
 @AutomaticFeature
 class ReflectionClasses implements Feature {
 
 	@Override
+	public void duringSetup(DuringSetupAccess access) {
+		System.loadLibrary("sunec");
+	}
+
+	@Override
 	public void beforeAnalysis(BeforeAnalysisAccess access) {
-		setupClasses();
+
+		// hikari datasource
+		SubstrateVM
+			.builder()
+			.constructors()
+			.clazz(Statement[].class)
+			.build();
+
+		// flyway migration
+		SubstrateVM
+			.builder()
+			.constructors()
+			.clazz(JavaUtilLogCreator.class)
+			.build();
+
+		SubstrateVM
+			.builder()
+			.fields()
+			.clazz(TransactionDefinition.class)
+			.build();
+
+		// thymeleaf
+		SubstrateVM
+			.builder()
+			.constructors()
+			.clazz(AdditionExpression.class)
+			.clazz(EqualsExpression.class)
+			.clazz(NotEqualsExpression.class)
+			.build();
+
 	}
 
-	/**
-	 * All classes defined here will have reflection support and be registered as spring beans
-	 */
-	static Class<?>[] getBeans(){
-		return new Class[]{
-		};
-	}
-
-	/**
-	 * All classes defined here will have reflection support
-	 */
-	static Class<?>[] getClasses(){
-		return new Class[]{
-			Statement[].class,
-//			FruitEntity.class
-		};
-	}
-
-	static void setupClasses() {
-		try {
-			System.out.println("> Loading classes for future reflection support");
-			for (final Class<?> clazz : getBeans()) {
-				process(clazz);
-			}
-			for (final Class<?> clazz : getClasses()) {
-				process(clazz);
-			}
-		} catch (Error e){
-			if(!e.getMessage().contains("The class ImageSingletons can only be used when building native images")){
-				throw e;
-			}
-		}
-	}
-
-	/**
-	 * Register all constructors and methods on graalvm to reflection support at runtime
-	 */
-	private static void process(Class<?> clazz) {
-		try {
-			System.out.println("> Declaring class: " + clazz.getCanonicalName());
-			RuntimeReflection.register(clazz);
-			for (final Method method : clazz.getMethods()) {
-				System.out.println("\t> method: " + method.getName() + "(" + method.getParameterCount() + ")");
-				RuntimeReflection.register(method);
-			}
-			for (final Constructor<?> constructor : clazz.getDeclaredConstructors()) {
-				System.out.println("\t> constructor: " + constructor.getName() + "(" + constructor.getParameterCount() + ")");
-				RuntimeReflection.register(constructor);
-			}
-		} catch (Exception e){
-			e.printStackTrace();
-		}
-	}
 }
-
