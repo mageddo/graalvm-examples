eae5f3eeb502fbd61aaa7cba4c2a7c9399ca7ed5
Elvis de Freitas <edigitalb@gmail.com>
2019-11-16 17:55:18 -0300

    reactivating in memory database

--- micronaut/test-commons/src/main/java/com/mageddo/tests/MicronautFactory.java
+++ micronaut/test-commons/src/main/java/com/mageddo/tests/MicronautFactory.java
@@ -1,6 +1,7 @@
 package com.mageddo.tests;
 
 import io.micronaut.context.ApplicationContext;
+import io.micronaut.context.annotation.Context;
 import io.micronaut.context.annotation.Factory;
 import io.micronaut.runtime.event.ApplicationStartupEvent;
 import io.micronaut.runtime.event.annotation.EventListener;
@@ -15,7 +16,7 @@ public class MicronautFactory {
 
 	private static ApplicationContext context;
 
-//	@Context
+	@Context
 	@SneakyThrows
 	public SingleInstancePostgresExtension setupEmbeddedDatabase() {
 		final SingleInstancePostgresExtension extension = EmbeddedPostgresExtension
