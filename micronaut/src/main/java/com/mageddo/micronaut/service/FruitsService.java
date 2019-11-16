822c5a931ba5f57f8f744846a348283bc6a6aa1b
Elvis de Freitas <edigitalb@gmail.com>
2019-11-16 10:26:08 -0300

    simplify class

--- micronaut/src/main/java/com/mageddo/micronaut/service/FruitsService.java
+++ micronaut/src/main/java/com/mageddo/micronaut/service/FruitsService.java
@@ -3,39 +3,33 @@ package com.mageddo.micronaut.service;
 import com.mageddo.micronaut.dao.FruitsDAO;
 import com.mageddo.micronaut.entity.FruitEntity;
 import io.micronaut.spring.tx.annotation.Transactional;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
+import lombok.RequiredArgsConstructor;
+import lombok.SneakyThrows;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.transaction.annotation.Propagation;
 
-import javax.inject.Inject;
 import javax.inject.Singleton;
 import java.util.List;
 
+@Slf4j
 @Singleton
+@RequiredArgsConstructor
 public class FruitsService {
 
 	private final FruitsDAO fruitsDAO;
-	private final Logger logger = LoggerFactory.getLogger(getClass());
-
-	public FruitsService(FruitsDAO fruitsDAO) {
-		this.fruitsDAO = fruitsDAO;
-	}
 
 	@Transactional(propagation = Propagation.REQUIRES_NEW)
 	public List<FruitEntity> getFruits()  {
 		fruitsDAO.traceSelect();
 		for (int i = 1; i <= 4; i++) {
-			logger.info("sleeping, second={}", i);
+			log.info("sleeping, second={}", i);
 			sleep();
 		}
 		return fruitsDAO.getFruits();
 	}
 
+	@SneakyThrows
 	private void sleep() {
-		try {
-			Thread.sleep(1000);
-		} catch (InterruptedException e) {
-			throw new RuntimeException(e);
-		}
+		Thread.sleep(1000);
 	}
 }
