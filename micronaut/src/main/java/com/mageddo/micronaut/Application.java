c7e5b026f634fc2542da1a24d57531206c2428c4
Elvis de Freitas <edigitalb@gmail.com>
2019-11-24 14:39:53 -0300

    setup some reflections

--- micronaut/src/main/java/com/mageddo/micronaut/Application.java
+++ micronaut/src/main/java/com/mageddo/micronaut/Application.java
@@ -1,14 +1,9 @@
 package com.mageddo.micronaut;
 
 import com.mageddo.micronaut.config.ApplicationContextUtils;
-import io.micronaut.context.ApplicationContext;
-import io.micronaut.context.annotation.Context;
-import io.micronaut.context.annotation.Factory;
 import io.micronaut.runtime.Micronaut;
 import io.micronaut.runtime.event.annotation.EventListener;
 import io.micronaut.runtime.server.event.ServerStartupEvent;
-
-import javax.annotation.PostConstruct;
 
 public class Application {
 
