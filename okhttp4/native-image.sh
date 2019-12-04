5f71da8069c6fe81f7471485b6a6cb6e4969a632
Elvis de Freitas <edigitalb@gmail.com>
2019-12-04 01:08:04 -0300

    config native-properties path

--- okhttp4/native-image.sh
+++ okhttp4/native-image.sh
@@ -1,2 +1,5 @@
-./gradlew clean build
-native-image --no-fallback --no-server -cp ./build/libs/graalvm-okhttp4.jar com.mageddo.graalvm.okhttp.Application
+#!/bin/bash
+
+set -e
+./gradlew clean build fatJar
+native-image --no-fallback --no-server -cp ./build/libs/graalvm-okhttp4-all.jar com.mageddo.graalvm.okhttp.Application
