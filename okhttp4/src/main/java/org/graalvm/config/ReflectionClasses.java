96435d4c18f650d03460aa4a993daa81e8cb78d0
Elvis de Freitas <edigitalb@gmail.com>
2019-12-08 13:32:10 -0300

    removing unused imports

--- okhttp4/src/main/java/org/graalvm/config/ReflectionClasses.java
+++ okhttp4/src/main/java/org/graalvm/config/ReflectionClasses.java
@@ -1,20 +1,14 @@
 package org.graalvm.config;
 
 import com.oracle.svm.core.annotate.AutomaticFeature;
-import okhttp3.Authenticator;
-import okhttp3.CookieJar;
-import okhttp3.Dns;
 import org.graalvm.nativeimage.hosted.Feature;
-import org.graalvm.nativeimage.hosted.RuntimeReflection;
 
 @AutomaticFeature
 class ReflectionClasses implements Feature {
-
 	@Override
 	public void duringSetup(DuringSetupAccess access) {
-		System.out.println("java.library.path=" + System.getProperty("java.library.path"));
+		System.out.printf("java.library.path=%s%n", System.getProperty("java.library.path"));
 		System.loadLibrary("sunec");
 	}
-
 }
 
