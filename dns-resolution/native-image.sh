e8fc4d8b50366da63702e730e6f7a2416de3ecb8
Elvis de Freitas <edigitalb@gmail.com>
2019-12-08 20:09:27 -0300

    setup execution

--- dns-resolution/native-image.sh
+++ dns-resolution/native-image.sh
@@ -9,4 +9,8 @@ native-image --no-server \
   -H:Name=graalvm-dns-resolution \
   -cp ./build/libs/graalvm-dns-resolution.jar
 
+echo "> works on my machine"
 ./build/graal/graalvm-dns-resolution
+
+echo "> but not at alpine image"
+docker run --rm -it --name tmp -v $PWD/build/graal/graalvm-dns-resolution:/graalvm-dns-resolution alpine:latest /graalvm-dns-resolution
