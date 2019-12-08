6294ef20756c85861ecac71f4391b8d5a9d2eeba
Elvis de Freitas <edigitalb@gmail.com>
2019-12-08 16:05:09 -0300

    configuring some reflection

--- bouncy-castle-over-ec/src/main/java/org/graalvm/config/ReflectionClasses.java
+++ bouncy-castle-over-ec/src/main/java/org/graalvm/config/ReflectionClasses.java
@@ -1,9 +1,11 @@
 package org.graalvm.config;
 
 import com.oracle.svm.core.annotate.AutomaticFeature;
+import nativeimage.Reflection;
 import org.graalvm.nativeimage.hosted.Feature;
 import org.graalvm.nativeimage.hosted.RuntimeReflection;
 
+//@Reflection(scanPackage = "org.bouncycastle.jcajce.provider", declaredMethods = true, declaredConstructors = true)
 @AutomaticFeature
 class ReflectionClasses implements Feature {
 
