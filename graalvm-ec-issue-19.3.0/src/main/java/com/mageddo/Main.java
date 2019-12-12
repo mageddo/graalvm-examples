142178c6969c2be2a62543625d91db3daaf48839
Elvis de Freitas <edigitalb@gmail.com>
2019-12-11 22:38:05 -0300

    printing providers

--- graalvm-ec-issue-19.3.0/src/main/java/com/mageddo/Main.java
+++ graalvm-ec-issue-19.3.0/src/main/java/com/mageddo/Main.java
@@ -2,16 +2,27 @@ package com.mageddo;
 
 import java.security.KeyPairGenerator;
 import java.security.NoSuchAlgorithmException;
+import java.security.Provider;
+import java.security.Security;
 import java.util.Base64;
 
 public class Main {
     public static void main(String[] args) throws NoSuchAlgorithmException {
+
+        System.out.print("providers=");
+        for (Provider provider : Security.getProviders()) {
+            System.out.printf("%s, ", provider.getName());
+        }
+        System.out.println();
+
         final var ec = KeyPairGenerator.getInstance("EC");
         ec.initialize(512);
         final var keyPair = ec.generateKeyPair();
         System.out.printf(
-            "algorithm=%s public key=%s%n",
-            keyPair.getPublic().getAlgorithm(), Base64.getEncoder().encodeToString(keyPair.getPublic().getEncoded())
+            "algorithm=%s, provider=%s, public key=%s%n",
+            keyPair.getPublic().getAlgorithm(),
+            ec.getProvider(),
+            Base64.getEncoder().encodeToString(keyPair.getPublic().getEncoded())
         );
     }
 }
