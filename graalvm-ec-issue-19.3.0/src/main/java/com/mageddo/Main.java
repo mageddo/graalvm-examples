38d5fe145400b3850c63c12c04d33de40c17497e
Elvis de Freitas <edigitalb@gmail.com>
2019-12-11 22:35:25 -0300

    implementing ec key pair example

--- graalvm-ec-issue-19.3.0/src/main/java/com/mageddo/Main.java
+++ graalvm-ec-issue-19.3.0/src/main/java/com/mageddo/Main.java
@@ -1,7 +1,17 @@
 package com.mageddo;
 
+import java.security.KeyPairGenerator;
+import java.security.NoSuchAlgorithmException;
+import java.util.Base64;
+
 public class Main {
-    public static void main(String[] args) {
-        System.out.println("Hello World!!!");
+    public static void main(String[] args) throws NoSuchAlgorithmException {
+        final var ec = KeyPairGenerator.getInstance("EC");
+        ec.initialize(512);
+        final var keyPair = ec.generateKeyPair();
+        System.out.printf(
+            "algorithm=%s public key=%s%n",
+            keyPair.getPublic().getAlgorithm(), Base64.getEncoder().encodeToString(keyPair.getPublic().getEncoded())
+        );
     }
 }
