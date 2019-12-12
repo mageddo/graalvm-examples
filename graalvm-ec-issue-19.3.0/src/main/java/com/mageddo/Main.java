851725b9d411a89b6398fbbe9603147388b44cb1
Elvis de Freitas <edigitalb@gmail.com>
2019-12-11 23:33:16 -0300

    fixing

--- graalvm-ec-issue-19.3.0/src/main/java/com/mageddo/Main.java
+++ graalvm-ec-issue-19.3.0/src/main/java/com/mageddo/Main.java
@@ -1,11 +1,20 @@
 package com.mageddo;
 
+import sun.security.ssl.SSLSocketFactoryImpl;
+
+import javax.net.ssl.SSLSocket;
+import java.io.IOException;
 import java.security.*;
 import java.util.Base64;
 
 public class Main {
-    public static void main(String[] args) throws NoSuchAlgorithmException {
+    public static void main(String[] args) throws Exception {
+        if(args.length != 0){
+            new Main().doStuff();
+        }
+    }
 
+    private void doStuff() throws Exception {
         System.out.print("providers=");
         for (Provider provider : Security.getProviders()) {
             System.out.printf("%s, ", provider.getName());
@@ -21,5 +30,13 @@ public class Main {
             ec.getProvider(),
             Base64.getEncoder().encodeToString(keyPair.getPublic().getEncoded())
         );
+
+
+//        final byte[] buff = new byte[100];
+        try(final SSLSocket socket = (SSLSocket) new SSLSocketFactoryImpl().createSocket("google.com", 443)){
+            socket.startHandshake();
+//            final int read = socket.getInputStream().read(buff);
+//            System.out.println(new String(buff, 0, read));
+        }
     }
 }
--- okhttp4/src/main/java/com/mageddo/graalvm/okhttp/Application.java
+++ okhttp4/src/main/java/com/mageddo/graalvm/okhttp/Application.java
@@ -1,12 +1,29 @@
 package com.mageddo.graalvm.okhttp;
 
-import org.graalvm.config.SslBuildConfig;
+import org.bouncycastle.jce.provider.BouncyCastleProvider;
+
+import java.security.Provider;
+import java.security.Security;
 
 public class Application {
+
+	static {
+		Security.addProvider(new BouncyCastleProvider());
+	}
+
 	public static void main(String[] args) {
-		SslBuildConfig.installTrustStore();
+//		SslBuildConfig.installTrustStore();
+
+		System.out.print("providers=");
+		for (Provider provider : Security.getProviders()) {
+			System.out.printf("%s, ", provider.getName());
+		}
+		System.out.println();
+
 		System.out.println(OkHttpGetter.get("https://google.com/robots.txt"));;
 		System.out.println(OkHttpGetter.get("https://acme.com/robots.txt"));;
 		System.out.println(OkHttpGetter.get("https://api.github.com/repos/square/okhttp/contributors"));;
+
+		System.out.println(Thread.getAllStackTraces().keySet());
 	}
 }
