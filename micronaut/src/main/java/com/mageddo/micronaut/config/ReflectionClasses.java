615d39e224b05bb3bbec6ae45763ba7bc75b7aa8
Elvis de Freitas <edigitalb@gmail.com>
2020-05-03 20:29:31 -0300

    fixing running

--- micronaut/src/main/java/com/mageddo/micronaut/config/ReflectionClasses.java
+++ micronaut/src/main/java/com/mageddo/micronaut/config/ReflectionClasses.java
@@ -3,14 +3,14 @@ package com.mageddo.micronaut.config;
 import com.oracle.svm.core.annotate.AutomaticFeature;
 import nativeimage.Reflection;
 import org.graalvm.nativeimage.hosted.Feature;
+import org.graalvm.nativeimage.hosted.RuntimeReflection;
 
 @Reflection(declaredConstructors = true, declaredMethods = true, scanPackage = "com.mageddo.micronaut.entity")
 @AutomaticFeature
 class ReflectionClasses implements Feature {
-
 	@Override
-	public void duringSetup(DuringSetupAccess access) {
-		System.loadLibrary("sunec");
+	public void duringAnalysis(DuringAnalysisAccess access) {
+		RuntimeReflection.register(com.zaxxer.hikari.util.ConcurrentBag.IConcurrentBagEntry[].class);
 	}
 
 }
