0b36ace2233ab945a1205364ace35280c77670fa
Elvis de Freitas <edigitalb@gmail.com>
2020-05-03 23:45:29 -0300

    simplifying config

--- micronaut/src/main/java/com/mageddo/micronaut/config/ReflectionClasses.java
+++ micronaut/src/main/java/com/mageddo/micronaut/config/ReflectionClasses.java
@@ -1,15 +1,9 @@
 package com.mageddo.micronaut.config;
 
 import com.oracle.svm.core.annotate.AutomaticFeature;
-import nativeimage.Reflection;
 import org.graalvm.nativeimage.hosted.Feature;
 
-@Reflection(declaredConstructors = true, declaredMethods = true, scanPackage = "com.mageddo.micronaut.entity")
+//@Reflection(declaredConstructors = true, declaredMethods = true, scanPackage = "com.mageddo.micronaut.entity")
 @AutomaticFeature
 class ReflectionClasses implements Feature {
-	@Override
-	public void duringAnalysis(DuringAnalysisAccess access) {
-//		RuntimeReflection.register(com.zaxxer.hikari.util.ConcurrentBag.IConcurrentBagEntry[].class);
-	}
-
 }
