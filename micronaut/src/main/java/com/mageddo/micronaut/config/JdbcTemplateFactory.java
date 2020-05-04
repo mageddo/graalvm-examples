89c0805a341e1687e4d0161ab2d9906806c9f505
Elvis de Freitas <edigitalb@gmail.com>
2020-05-03 23:43:55 -0300

    simplifying config

--- micronaut/src/main/java/com/mageddo/micronaut/config/JdbcTemplateFactory.java
+++ micronaut/src/main/java/com/mageddo/micronaut/config/JdbcTemplateFactory.java
@@ -1,11 +1,8 @@
 package com.mageddo.micronaut.config;
 
-import com.zaxxer.hikari.HikariDataSource;
-import io.micronaut.context.annotation.EachBean;
 import io.micronaut.context.annotation.Factory;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.jdbc.core.namedparam.NamedParameterJdbcTemplate;
-import org.springframework.jdbc.datasource.DataSourceTransactionManager;
 
 import javax.inject.Singleton;
 import javax.sql.DataSource;
@@ -21,34 +18,6 @@ public class JdbcTemplateFactory {
 	@Singleton
 	public NamedParameterJdbcTemplate namedJdbcTemplate(DataSource dataSource) {
 		return new NamedParameterJdbcTemplate(dataSource);
-	}
-
-	@Singleton
-	public DataSourceTransactionManager DataSourceTransactionManager(DataSource dataSource) {
-		return new DataSourceTransactionManager(dataSource);
-	}
-
-	@EachBean(DatasourceConfiguration.class)
-	public DataSource dataSource(DatasourceConfiguration config){
-		final HikariDataSource dc = new HikariDataSource();
-		dc.setDriverClassName(config.getDriverClassName());
-		dc.setMinimumIdle(config.getMinimumIdle());
-		dc.setMaximumPoolSize(config.getMaximumPoolSize());
-		dc.setTransactionIsolation(config.getTransactionIsolation());
-		dc.setAutoCommit(config.isAutoCommit());
-		dc.setConnectionTestQuery(config.getConnectionTestQuery());
-//		dc.setHealthCheckRegistry();
-//		dc.setLoginTimeout();
-//		dc.setConnectionTimeout();
-		dc.setInitializationFailTimeout(config.getInitializationFailTimeout());
-		dc.setJdbcUrl(config.getJdbcUrl());
-//		dc.setConnectionInitSql();
-//		dc.setIdleTimeout();
-		dc.setPassword(config.getPassword());
-		dc.setUsername(config.getUsername());
-		dc.setPoolName("Elvis Pool");
-//		dc.setSchema();
-		return dc;
 	}
 
 }
--- micronaut/src/main/java/com/mageddo/micronaut/config/ReflectionClasses.java
+++ micronaut/src/main/java/com/mageddo/micronaut/config/ReflectionClasses.java
@@ -1,15 +1,15 @@
 package com.mageddo.micronaut.config;
 
 import com.oracle.svm.core.annotate.AutomaticFeature;
+import nativeimage.Reflection;
 import org.graalvm.nativeimage.hosted.Feature;
-import org.graalvm.nativeimage.hosted.RuntimeReflection;
 
-//@Reflection(declaredConstructors = true, declaredMethods = true, scanPackage = "com.mageddo.micronaut.entity")
+@Reflection(declaredConstructors = true, declaredMethods = true, scanPackage = "com.mageddo.micronaut.entity")
 @AutomaticFeature
 class ReflectionClasses implements Feature {
 	@Override
 	public void duringAnalysis(DuringAnalysisAccess access) {
-		RuntimeReflection.register(com.zaxxer.hikari.util.ConcurrentBag.IConcurrentBagEntry[].class);
+//		RuntimeReflection.register(com.zaxxer.hikari.util.ConcurrentBag.IConcurrentBagEntry[].class);
 	}
 
 }
