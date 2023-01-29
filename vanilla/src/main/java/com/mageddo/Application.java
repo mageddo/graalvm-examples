9114bdff3ca8b492b6fbb319efb67dccd9adeef3
Elvis de Freitas <edigitalb@gmail.com>
2023-01-29 02:12:43 -0300

    updating files

--- vanilla/src/main/java/com/mageddo/Application.java
+++ vanilla/src/main/java/com/mageddo/Application.java
@@ -11,27 +11,27 @@ import java.nio.file.Paths;
 
 public class Application {
 
-	private static final Logger LOG = LoggerFactory.getLogger(Application.class);
+  private static final Logger LOG = LoggerFactory.getLogger(Application.class);
 
-	public static void main(String[] args) throws Exception {
-		LOG.info("Hello World!!!");
-	}
+  public static void main(String[] args) throws Exception {
+    LOG.info("Hello World!!!");
+  }
 
-	private static void fileReadingPerformanceCheck() throws IOException {
-		StopWatch stopWatch = new StopWatch();
-		stopWatch.start();
-		Runtime.getRuntime().exec("truncate --size 4G /tmp/file");
-		try(final InputStream in = Files.newInputStream(Paths.get("/tmp/file"));){
-			final byte[] buff = new byte[8124];
-			int i = 0;
-			while (in.read(buff) != -1){
-				i++;
-				int x = 5 * buff[0];
-				if(i % 100000 == 0){
-					LOG.info("i={}", i);
-				}
-			}
-		}
-		LOG.info("done time={}", stopWatch.getTime());
-	}
+  private static void fileReadingPerformanceCheck() throws IOException {
+    StopWatch stopWatch = new StopWatch();
+    stopWatch.start();
+    Runtime.getRuntime().exec("truncate --size 4G /tmp/file");
+    try (final InputStream in = Files.newInputStream(Paths.get("/tmp/file"));) {
+      final byte[] buff = new byte[8124];
+      int i = 0;
+      while (in.read(buff) != -1) {
+        i++;
+        int x = 5 * buff[0];
+        if (i % 100000 == 0) {
+          LOG.info("i={}", i);
+        }
+      }
+    }
+    LOG.info("done time={}", stopWatch.getTime());
+  }
 }
