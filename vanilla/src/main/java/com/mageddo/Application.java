6577ce456fa1b267261632832655462118ed5462
Elvis de Freitas <edigitalb@gmail.com>
2023-01-29 02:32:10 -0300

    Fully working

--- vanilla/src/main/java/com/mageddo/Application.java
+++ vanilla/src/main/java/com/mageddo/Application.java
@@ -4,8 +4,10 @@ import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.databind.SerializationFeature;
 import com.mageddo.entrypoint.FruitResource;
 import lombok.extern.slf4j.Slf4j;
+import nativeimage.Reflection;
 
 @Slf4j
+@Reflection(declaredConstructors = true, declaredMethods = true, scanPackage = "com.mageddo.entrypoint")
 public class Application {
 
   public static void main(String[] args) throws Exception {
