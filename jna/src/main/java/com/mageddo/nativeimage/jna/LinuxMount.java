034d48ef1d579f6d628047e80e6920406601dff7
Elvis de Freitas <edigitalb@gmail.com>
2023-02-19 13:54:01 -0300

    Ajusting reflection

--- jna/src/main/java/com/mageddo/nativeimage/jna/LinuxMount.java
+++ jna/src/main/java/com/mageddo/nativeimage/jna/LinuxMount.java
@@ -50,29 +50,4 @@ public class LinuxMount {
     MountContent.ByReference getmntent(Pointer FILE);
   }
 
-  public static class MountContent extends Structure {
-
-    public String mnt_fsname;
-    public String mnt_dir;
-    public String mnt_type;
-    public String mnt_opts;
-    public int mnt_freq;
-    public int mnt_passno;
-
-    @Override
-    protected List getFieldOrder() {
-      List<String> fieds = new ArrayList<>();
-      for (final Field f : MountContent.class.getDeclaredFields()) {
-        if (!f.isSynthetic())
-          fieds.add(f.getName());
-      }
-      return fieds;
-    }
-
-    public static class ByReference extends MountContent implements Structure.ByReference {
-    }
-
-    public static class ByValue extends MountContent implements Structure.ByValue {
-    }
-  }
 }
