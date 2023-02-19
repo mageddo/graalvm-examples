f98fa1a26835228690a55449618ef3fa67d0a246
Elvis Souza <edigitalb@gmail.com>
2023-02-19 13:56:05 -0300

    JNA - More samples (#19)
    * initial commit
    
    * Cleanup
    
    * Hello world is working
    
    * new try
    
    * Fixing links
    
    * Fully working
    
    * new sample
    
    * Ajusting reflection
    
    * Fixing compiling errors

--- jna/src/main/java/com/mageddo/nativeimage/jna/LinuxMount.java
+++ jna/src/main/java/com/mageddo/nativeimage/jna/LinuxMount.java
@@ -1,12 +1,13 @@
 package com.mageddo.nativeimage.jna;
 
+import com.mageddo.nativeimage.jna.struct.MountContent;
 import com.sun.jna.Library;
 import com.sun.jna.Native;
 import com.sun.jna.Pointer;
-import com.sun.jna.Structure;
 import org.apache.commons.lang3.Validate;
 
-import java.lang.reflect.Field;
+import java.io.IOException;
+import java.io.UncheckedIOException;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -23,9 +24,23 @@ public class LinuxMount {
     return String.format("%s - %d\n", new String(hostname), returnCode);
   }
 
+  public static List<MountContent> findMounts() {
+    final var mtabPath = "/etc/mtab";
+    final var mountFile = CLibrary.INSTANCE.fopen(mtabPath, "r");
+    if (mountFile == null) {
+      throw new UncheckedIOException(new IOException("File not exists: " + mtabPath));
+    }
+    MountContent.ByReference mtent;
+    final var mounts = new ArrayList<MountContent>();
+    while ((mtent = CLibrary.INSTANCE.getmntent(mountFile)) != null) {
+      mounts.add(mtent);
+    }
+    return mounts;
+  }
+
   public interface CLibrary extends Library {
 
-    CLibrary INSTANCE = (CLibrary) Native.loadLibrary("c", CLibrary.class);
+    CLibrary INSTANCE = Native.loadLibrary("c", CLibrary.class);
 
     int gethostname(byte[] name, int nameLength);
 
@@ -34,45 +49,4 @@ public class LinuxMount {
     MountContent.ByReference getmntent(Pointer FILE);
   }
 
-  public static void main(String[] args) {
-
-
-//		final String nfsPath = "/etc/mtab";
-//		final Pointer mountFile = CLibrary.INSTANCE.fopen(nfsPath, "r");
-//		if (mountFile == null) {
-//			System.err.println("File not exists: " + nfsPath);
-//			System.exit(-1);
-//		}
-//		MountContent.ByReference mtent;
-//		while ((mtent = CLibrary.INSTANCE.getmntent(mountFile)) != null) {
-//			System.out.println(mtent);
-//		}
-
-  }
-
-  public static class MountContent extends Structure {
-
-    public String mnt_fsname;
-    public String mnt_dir;
-    public String mnt_type;
-    public String mnt_opts;
-    public int mnt_freq;
-    public int mnt_passno;
-
-    @Override
-    protected List getFieldOrder() {
-      List<String> fieds = new ArrayList<>();
-      for (final Field f : MountContent.class.getDeclaredFields()) {
-        if (!f.isSynthetic())
-          fieds.add(f.getName());
-      }
-      return fieds;
-    }
-
-    public static class ByReference extends MountContent implements Structure.ByReference {
-    }
-
-    public static class ByValue extends MountContent implements Structure.ByValue {
-    }
-  }
 }
--- jna/src/main/java/com/mageddo/nativeimage/jna/Main.java
+++ jna/src/main/java/com/mageddo/nativeimage/jna/Main.java
@@ -4,7 +4,21 @@ public class Main {
   public static void main(String[] args) {
     System.out.println("Aqui começa mais uma história de amor!");
 
+    System.out.println("> Hostname Find");
     final var hostname = LinuxMount.findHostname();
     System.out.printf("hostname=%s", hostname);
+
+    System.out.println("------------------------------");
+    System.out.println();
+
+    System.out.println("> Linux Mounts Find");
+    final var firstMount = LinuxMount.findMounts()
+      .stream()
+      .findFirst()
+      .orElse(null);
+    System.out.printf("First Mount Found: %s%n", firstMount);
+
+    System.out.println("------------------------------");
+    System.out.println();
   }
 }
