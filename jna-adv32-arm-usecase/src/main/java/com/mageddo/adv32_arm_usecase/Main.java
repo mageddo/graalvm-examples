74aea44aa176eeb4c161733344bfd29aa46e0cce
Elvis de Freitas <edigitalb@gmail.com>
2023-03-09 00:37:47 -0300

    more detaiçs

--- jna-adv32-arm-usecase/src/main/java/com/mageddo/adv32_arm_usecase/Main.java
+++ jna-adv32-arm-usecase/src/main/java/com/mageddo/adv32_arm_usecase/Main.java
@@ -1,10 +1,12 @@
 package com.mageddo.adv32_arm_usecase;
 
+import com.sun.jna.platform.unix.LibC;
 import com.sun.jna.platform.win32.Advapi32Util;
 import com.sun.jna.platform.win32.WinReg;
 
 public class Main {
   public static void main(String[] args) {
+    System.out.println(LibC.INSTANCE.getenv("PATH"));
     final String key = "SYSTEM\\CurrentControlSet\\Control\\FileSystem";
     final String value = Advapi32Util.registryGetStringValue(WinReg.HKEY_LOCAL_MACHINE, key, "LongPathsEnabled");
     System.out.println(value);
