1b1d02cc11d51c7cd9e18152ab078d1d807bd91c
Elvis de Freitas <edigitalb@gmail.com>
2023-03-09 13:15:55 -0300

    working concept

--- jna-adv32-arm-usecase/src/main/java/com/mageddo/adv32_arm_usecase/Main.java
+++ jna-adv32-arm-usecase/src/main/java/com/mageddo/adv32_arm_usecase/Main.java
@@ -5,9 +5,13 @@ import com.sun.jna.platform.win32.Advapi32Util;
 import com.sun.jna.platform.win32.WinReg;
 
 public class Main {
+
   public static void main(String[] args) {
     System.out.println(LibC.INSTANCE.getenv("PATH"));
     final String key = "SYSTEM\\CurrentControlSet\\Control\\FileSystem";
+
+    System.out.println(SecondClass.KERNEL_32);
+
     final String value = Advapi32Util.registryGetStringValue(WinReg.HKEY_LOCAL_MACHINE, key, "LongPathsEnabled");
     System.out.println(value);
   }
---
+++ jna-adv32-arm-usecase/src/main/java/com/mageddo/adv32_arm_usecase/SecondClass.java
@@ -0,0 +1,7 @@
+package com.mageddo.adv32_arm_usecase;
+
+import com.sun.jna.platform.win32.Kernel32;
+
+public class SecondClass {
+  static Kernel32 KERNEL_32 = Kernel32.INSTANCE;
+}
